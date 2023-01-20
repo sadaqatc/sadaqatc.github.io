9dc655ce889d5c68e909ac67d7bfb426e80b19fc
sadaqatc <sadaqat.chaudhry@dcmail.ca>
2023-01-19 20:09:23 -0500

    final commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -7,6 +7,22 @@
             location.href = "about.html"
         });
     }
+    let MainContent = document.getElementsByTagName("main")[0];
+    let MainParagraph = document.createElement("p")
+    MainParagraph.setAttribute("id", "MainParagraph")
+    MainParagraph.setAttribute("class", "mt-3")
+    MainParagraph.textContent = "This is the main Paragraph!"
+    MainContent.appendChild(MainParagraph)
+    let FirstString = "This is";
+    let SecondString =  '${FirstString} the Main Paragraph.' ;
+    MainParagraph.textContent = SecondString;
+    //Create Article
+    let Article = document.createElement("article");
+    let ArticleParagraph = '<p id="ArticleParagraph" class="mt-3"> This is my article paragraph </p>';
+    Article.setAttribute("class", "container")
+    Article.innerHTML = ArticleParagraph;
+    DocumentBody.appendChild(Article);
+
     function DisplayProductsPage(){
 
     }
